e4ac13d97d78fde86e72566cc37df81e1318d08d
ataman <atamanenkoalex90@gmail.com>
2023-04-26 00:49:09 +0300

    Add index.html

--- university/teacher/urls.py
+++ university/teacher/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("teachers/", views.teachers, name="teachers"),
     path("generate-teachers/", views.generate_teachers, name="generate_teachers"),
 ]
